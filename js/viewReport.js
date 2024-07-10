023f0a480aedb44c04e8aa95f757ff88a46261fb
Waji <wajihahzaidi2@gmail.com>
2024-07-10 14:23:34 +0800

    Update view report

--- js/viewReport.js
+++ js/viewReport.js
@@ -1,30 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() { 
+document.addEventListener('DOMContentLoaded', () => {
     // Get the modal
     var modal = document.getElementById("viewReportModal");
 
-    // Get the buttons that open the modal
-    var viewButtons = document.querySelectorAll(".view-report-btn");
-
     // Get the <span> element that closes the modal
-    var closeModal = document.getElementById("closeViewReportModal");
+    var closeEditDeviceModal = document.getElementById("closeViewReportModal");
 
-    // When the user clicks the button, open the modal 
-    viewButtons.forEach(button => {
-        button.addEventListener("click", function(event) {
-            event.preventDefault();
+    // Add click event listeners to all edit buttons
+    document.querySelectorAll('.view-report-btn').forEach((btn) => {
+        btn.addEventListener('click', function() {
             modal.style.display = "block";
         });
     });
 
     // When the user clicks on <span> (x), close the modal
-    closeModal.addEventListener("click", function() {
+    closeEditDeviceModal.onclick = function() {
         modal.style.display = "none";
-    });
+    };
 
     // When the user clicks anywhere outside of the modal, close it
-    window.addEventListener("click", function(event) {
+    window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
-    });
+    };
+
 });
