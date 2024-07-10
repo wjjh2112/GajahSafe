f7b1533e0daabae76f5dd67b2d46b53671e9c7b3
Waji <wajihahzaidi2@gmail.com>
2024-07-10 13:52:19 +0800

    Add viewReport.js

---
+++ js/viewReport.js
@@ -0,0 +1,28 @@
+    // Get the modal
+    var modal = document.getElementById("viewReportModal");
+
+    // Get the button that opens the modal
+    var viewButtons = document.querySelectorAll(".view-report-btn");
+
+    // Get the <span> element that closes the modal
+    var closeModal = document.getElementById("closeViewReportModal");
+
+    // When the user clicks the button, open the modal 
+    viewButtons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            event.preventDefault();
+            modal.style.display = "block";
+        });
+    });
+
+    // When the user clicks on <span> (x), close the modal
+    closeModal.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.addEventListener("click", function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
