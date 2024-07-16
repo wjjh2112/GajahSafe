9b1da1aed2b374919fab20c89e09df4c862ed14c
Waji <wajihahzaidi2@gmail.com>
2024-07-16 20:17:00 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware to parse JSON
-app.use(express.json());
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/dummydb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Failed to connect to MongoDB', err);
+});
 
-// Basic route
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files (if any)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
 app.get('/', (req, res) => {
-  res.send('Hello, GajahSafe!');
+  res.send('Hello World!');
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
