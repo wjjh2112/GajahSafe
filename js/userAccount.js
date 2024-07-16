753e4a67bb5840b58cf0fd3f4f693adca710e8ad
Waji <wajihahzaidi2@gmail.com>
2024-07-16 14:25:50 +0800

    Revert "Update user displays script"
    This reverts commit 39a83410042cc1a461e682b6e6c846a17bb14fd0.

--- js/userAccount.js
+++ js/userAccount.js
@@ -1,19 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.onload = () => {
     const user = JSON.parse(localStorage.getItem('user'));
-
+    
     if (user) {
-      // Update the account section with user information
       document.getElementById('userName').textContent = user.fullname;
       document.getElementById('dropdownUserName').textContent = user.fullname;
       document.getElementById('dropdownUserEmail').textContent = user.email;
-      // Optionally, update the avatar image based on user information
     } else {
-      // Redirect to login page if no user information is found
-      window.location.href = 'login.html';
+      window.location.href = 'login.html'; // Redirect to login if no user data
     }
-  });
-
-  document.getElementById('logoutBtn').addEventListener('click', () => {
-    localStorage.removeItem('user');
-    window.location.href = 'login.html'; // Redirect to login page on logout
-  });
+  };
+  
