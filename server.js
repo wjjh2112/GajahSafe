64b921fee20c6d4323965a4d9e2568c1714f39f4
Waji <wajihahzaidi2@gmail.com>
2024-07-16 23:13:41 +0800

    Revert "Update remove hash password for testing purposes"
    This reverts commit 65ea1e1af53828d11a35582989845e3a910bd49f.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const session = require('express-session');
+const bcrypt = require('bcryptjs');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -19,7 +20,8 @@ mongoose.connect('mongodb://localhost:27017/dummydb', {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
-  secret: '10c31eb407c92a61755317c61114e93490c77ac6082b33824b4b29c3b2d2ee91a29b3b4f5e7abce2d37cec4bc0271a54c60df01e813327a18a8a53aecae3dd9b',  resave: false,
+  secret: '10c31eb407c92a61755317c61114e93490c77ac6082b33824b4b29c3b2d2ee91a29b3b4f5e7abce2d37cec4bc0271a54c60df01e813327a18a8a53aecae3dd9b',
+  resave: false,
   saveUninitialized: true
 }));
 
@@ -39,13 +41,22 @@ app.post('/login', async (req, res) => {
   const { email, password } = req.body;
   const user = await mongoose.model('User').findOne({ email });
 
-  if (user && user.password === password) {
+  if (user && await bcrypt.compare(password, user.password)) {
     req.session.user = { name: user.fullname, email: user.email };
     res.redirect('/');
   } else {
     res.status(401).send('Invalid credentials');
   }
 });
+
+app.get('/user-info', (req, res) => {
+  if (req.session.user) {
+      res.json(req.session.user);
+  } else {
+      res.status(401).json(null);
+  }
+});
+
 
 app.get('/logout', (req, res) => {
   req.session.destroy(err => {
