207c0f1d412fabc23f7bca57a648754d80a24121
Waji <wajihahzaidi2@gmail.com>
2024-07-18 15:23:52 +0800

    Revert back

--- server.js
+++ server.js
@@ -25,19 +25,95 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname)));
 
 // Routes
-const indexRoutes = require('./routes/index');
-const authRoutes = require('./routes/auth');
-const usersRoutes = require('./routes/users');
-const camerasRoutes = require('./routes/cameras');
-const electricFencesRoutes = require('./routes/electricFences');
-const devicesRoutes = require('./routes/devices');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-app.use('/', indexRoutes);
-app.use('/auth', authRoutes);
-app.use('/users', usersRoutes);
-app.use('/cameras', camerasRoutes);
-app.use('/electricFences', electricFencesRoutes);
-app.use('/addDevice', devicesRoutes);
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  // Find user in database
+  mongoose.connection.db.collection('users').findOne({ email, password }, (err, user) => {
+      if (err) {
+          return res.status(500).json({ error: 'Internal server error' });
+      }
+      if (!user) {
+          return res.status(401).json({ error: 'Invalid email or password' });
+      }
+      // Return user data including usertype as JSON
+      res.json({
+          name: user.fullname,
+          email: user.email,
+          avatar: 'images/icon/avatar-01.jpg', // Example static avatar path
+          usertype: user.usertype // Include usertype
+      });
+  });
+});
+
+// Endpoint to fetch all users
+app.get('/users', (req, res) => {
+  mongoose.connection.db.collection('users').find({}).toArray((err, users) => {
+      if (err) {
+          return res.status(500).json({ error: 'Internal server error' });
+      }
+      res.json(users);
+  });
+});
+
+// Endpoint to fetch all electric fences
+app.get('/electricFences', (req, res) => {
+  mongoose.connection.db.collection('electricFences').find({}).toArray((err, fences) => {
+    if (err) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+    res.json(fences);
+  });
+});
+
+// Endpoint to fetch all cameras
+app.get('/cameras', (req, res) => {
+  mongoose.connection.db.collection('cameras').find({}).toArray((err, cameras) => {
+    if (err) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+    res.json(cameras);
+  });
+});
+
+// Endpoint to add a new device
+app.post('/addDevice', (req, res) => {
+  const deviceType = req.body['device-type'];
+  let typePrefix;
+
+  if (deviceType === 'Camera') {
+    typePrefix = 'cam';
+  } else if (deviceType === 'Electric Fence') {
+    typePrefix = 'ef';
+  } else {
+    return res.status(400).json({ error: 'Invalid device type' });
+  }
+
+  const deviceData = {
+    [`${typePrefix}_id`]: req.body['device-id'],
+    [`${typePrefix}Name`]: req.body['device-name'],
+    [`${typePrefix}Location`]: req.body['device-location'],
+    [`${typePrefix}Lat`]: req.body['device-latitude'],
+    [`${typePrefix}Long`]: req.body['device-longitude'],
+    [`${typePrefix}Stat`]: req.body.status
+  };
+
+  const collectionName = deviceType === 'Camera' ? 'cameras' : 'electricFences';
+
+  mongoose.connection.db.collection(collectionName).insertOne(deviceData, (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to add device' });
+    }
+    res.status(200).json({ success: true, message: 'Device added successfully' });
+  });
+});
 
 // Start the server
 app.listen(port, () => {
