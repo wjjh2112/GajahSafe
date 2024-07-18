47b621512fcf8a83d4c4cbf7d7b67a5a9dafde16
Waji <wajihahzaidi2@gmail.com>
2024-07-18 15:52:55 +0800

    Update

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  user_id: String,
   fullname: String,
   email: String,
   password: String,
