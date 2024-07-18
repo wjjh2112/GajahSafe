f2b16a35367dedef78d1734cf797553e80f0ef8d
Waji <wajihahzaidi2@gmail.com>
2024-07-18 15:57:41 +0800

    Revert "Update"
    This reverts commit 47b621512fcf8a83d4c4cbf7d7b67a5a9dafde16.

--- models/user.js
+++ models/user.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  user_id: String,
   fullname: String,
   email: String,
   password: String,
