68b07ca559a216e6364e29c56e9dcb5fbfd3a255
Waji <wajihahzaidi2@gmail.com>
2024-07-31 16:46:34 +0800

    Update rm time in date and time

--- js/addReport.js
+++ js/addReport.js
@@ -22,13 +22,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Date & Time JS
 document.addEventListener("DOMContentLoaded", function() {
-    var dateTimeInput = document.getElementById('datetime-input');
-    var currentDateTime = new Date();
-    var formattedDateTime = currentDateTime.toISOString().slice(0, 16); // Format the date and time for input[type="datetime-local"]
+    var dateInput = document.getElementById('date-input');
+    var currentDate = new Date().toISOString().slice(0, 10); // Format the date for input[type="date"]
 
-    dateTimeInput.value = formattedDateTime;
-    dateTimeInput.readOnly = true; // Make the input read-only
+    dateInput.value = currentDate;
+    dateInput.readOnly = true; // Make the input read-only
 });
+
 
 // Upload Images File JS
 const fileArray = [];
