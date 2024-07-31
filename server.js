df8cc52f50c2782f35f9122da856d395dba82ade
Waji <wajihahzaidi2@gmail.com>
2024-07-31 23:48:11 +0800

    Update

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const uploadFile = (filePath, fileName) => {
     Bucket: 'gajahsafe-report-images', // e.g., 'my-gajahsafe-bucket'
     Key: fileName, // File name you want to save as in S3
     Body: fileContent,
-    ContentType: 'image/jpeg', // Set the correct MIME type for the image
+    ContentType: 'image/png', // Set the correct MIME type for the image
     ContentDisposition: 'inline' // This will instruct the browser to display the image inline
   };
 
